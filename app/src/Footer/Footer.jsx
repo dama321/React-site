f225855ec5b8c043edfd4bfd9891d1588bb81752
dama321 <gd.kalykova@gmail.com>
2022-10-21 23:37:31 +0600

    first commit

---
+++ app/src/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer/Footer.css";
+
+function Footer() {
+  return (
+    <div className="Footer-site-footer">
+      <b>React site YELLOWAGENCY.KG</b>
+      <p>2022 copyring. Все права защищены</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ app/src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header-site-header">
+      <h1>React site</h1>
+      <h2>slogan YELLOWAGENCY.KG</h2>
+    </div>
+  );
+}
+
+export default Header;
